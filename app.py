5d724558433a7dc1ddd4a2bf645777504cd5f20c
ehdgks0627 <ehdgks0627@naver.com>
2019-11-29 14:25:24 +0900

    A

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
 from flask import Flask, render_template, send_from_directory
 from typeform import TypeForm
 from insure_list import insure_list
+import random
 
 app = Flask(__name__, static_url_path='/static/', static_folder='templates/static')
 typeform = TypeForm()
 
+TRANSLATE = {
+    'brain': '뇌혈관질환',
+    'breast': '유방암',
+    'colorectal': '대장암',
+    'genital': '생식기암',
+    'heart': '심장질환',
+    'liver': '간암',
+    'lung': '폐암',
+    'stomach': '위암',
+    'thyroid': '갑상선암',
+    'urinary': '비뇨기과암'
+}
+
 
 @app.route('/', methods=['GET'])
 def index():
-    TRANSLATE = {
-        'brain': '뇌혈관질환',
-        'breast': '유방암',
-        'colorectal': '대장암',
-        'genital': '생식기암',
-        'heart': '심장질환',
-        'liver': '간암',
-        'lung': '폐암',
-        'stomach': '위암',
-        'thyroid': '갑상선암',
-        'urinary': '비뇨기과암'
-    }
+    recommend = insure_list[:]
+    random.shuffle(recommend)
     context = {
         'age': 20,
         'gender': '남성',
@@ -47,7 +51,7 @@ def index():
             'thyroid': 85,
             'urinary': 20
         },
-        'recommand': [],
+        'recommend': recommend[:5],
         'TRANSLATE': TRANSLATE
     }
     return render_template('index.html', **context)
